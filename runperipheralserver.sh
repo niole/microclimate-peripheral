039a47deb7f0572d3bc7b07cc149f98459a15daa
niole <niolenelson@gmail.com>
2020-12-10 03:13:29 +0000

    Logs all go to tmp, can pair perf

--- runperipheralserver.sh
+++ runperipheralserver.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-nohup ./setup_peripheral.py > peripheral.log 2>&1 &
+nohup ./setup_peripheral.py > /tmp/peripheral.log 2>&1 &
 
 echo $! > save_setup_peripheral_pid.txt
--- start_all.sh
+++ start_all.sh
@@ -2,7 +2,7 @@
 
 python3.5 -m pip install -r requirements.txt
 
-nohup ./runpi.sh > led.log 2>&1 &
+nohup ./runpi.sh > /tmp/led.log 2>&1 &
 echo $! > save_pid.txt
 
 ./runperipheralserver.sh
